2fffac25080ab4b5792501e85f4591c68c00facb
kgy <gy8094@naver.com>
2024-07-01 13:25:07 +0900

    init

---
+++ src/main/java/grapa/cp/cpdemo/CpdemoApplication.java
@@ -0,0 +1,13 @@
+package grapa.cp.cpdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CpdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CpdemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/grapa/cp/cpdemo/CpdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package grapa.cp.cpdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CpdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
